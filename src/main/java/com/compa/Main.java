9536914fcd0134541141e8de2a4edfcd0a8bc904
Vincent <giardina.v@gmail.com>
2018-02-16 16:35:49 +0100

    Vertx is running

---
+++ src/main/java/com/compa/Main.java
@@ -0,0 +1,14 @@
+package com.compa;
+
+import io.vertx.core.AbstractVerticle;
+
+public class Main extends AbstractVerticle {
+
+    @Override
+    public void start() {
+        vertx.createHttpServer()
+                .requestHandler(req -> req.response().end("Hello Vert.x!"))
+                .listen(8080);
+    }
+
+}
