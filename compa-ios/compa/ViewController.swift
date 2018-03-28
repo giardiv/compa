76b0895c071eb47b15784365c80ee35a4fb4add3
Sarah <mouffoksarah1@gmail.com>
2018-03-28 16:09:06 +0200

    first attempt at api calls

--- compa-ios/compa/ViewController.swift
+++ compa-ios/compa/ViewController.swift
@@ -10,15 +10,65 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    enum Method :String {
+        case PUT
+        case POST
+        case DELETE
+        case GET
+    }
+    
+    let session = URLSession.shared //singleton with default behavior
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+      
+        GETAPIRequest(
+            url: "https://jsonplaceholder.typicode.com/posts/1",
+            success: { data in
+                print(data)
+
+            },
+            error: { data in
+                print(data)
+            })
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
+    
+    
+    func GETAPIRequest(url : String, success: @escaping (_ data: Dictionary<String, AnyObject> )->Void, error: @escaping (_ data: Error )->Void ) {
+        return APIRequest(url: url, method: Method.GET.rawValue, successHandler:success, errorHandler:error);
+    }
+    
+    private func APIRequest(url:String, method: String, successHandler: @escaping (_ data: Dictionary<String, AnyObject> )->Void, errorHandler: @escaping (_ data: Error )->Void)  {
+       
+        var request = URLRequest(url: URL(string: url)!)
+        request.httpMethod = method
+
+        
+        let task = session.dataTask(with: request, completionHandler: { data, response, error -> Void in
+            if((error) != nil){
+               errorHandler(error!)
+            }
+            else{
+                do {
+                    let json = try JSONSerialization.jsonObject(with: data!) as! Dictionary<String, AnyObject>
+                    successHandler(json)
+                } catch {
+                    //call error handler with error instance ? or maybe throw 
+                }
+            }
+            
+        })
+        
+        task.resume()
+        
+       
+    }
+    
 
 
 }
